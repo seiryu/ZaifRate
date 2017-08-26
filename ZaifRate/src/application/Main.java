bb9ec55bf585ee427d7777e7cee0ddc58a263e74
Seiryu <Seiryu@DESKTOP-K53MSPT>
2017-08-26 20:06:03 +0900

    View.fxmlに変更２

--- ZaifRate/src/application/Main.java
+++ ZaifRate/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
